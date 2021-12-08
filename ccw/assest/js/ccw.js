fe8ebe7633999fad25f5a707ed636e1a95c6ab04
Yi-Cong, Kuo <yicongk@igis.com.tw>
2021-12-08 13:41:07 +0800

    add ccw.js
    建立原因
    - 使用 esri calcite web 改版官網

---
+++ ccw/assest/js/ccw.js
@@ -0,0 +1,41 @@
+(function (global){
+    // 命名空間
+    var namespace = function (ns_string) {
+        var parts = ns_string.split('.'), 
+            parent = global, 
+            i;
+        if (parts[0] === "global") {
+            parts = parts.slice(1);       
+        }
+
+        for (i = 0; i < parts.length; i++){
+            // 若沒有這物件沒有這個屬性 則視為空物件
+            if (typeof parent[parts[i]] === "undefined") {
+                parent[parts[i]] = {};
+            }
+            parent = parent[parts[i]];
+        }
+        return parent;
+    };
+
+    var addOnLoadEvent = function (func) {
+        var oldonload = global.onload;
+        
+        if ( typeof global.onload != "function" ) {
+            global.onload = func;
+        }
+        else {
+            global.onload = function() {
+                if (oldonload) oldonload();
+                func();
+            };
+        }
+    };
+
+    // ============================
+    //      Export Modules
+    // ============================
+    var ccw = namespace('global.ccw');
+    	ccw.namespace = namespace;
+        ccw.addOnLoadEvent = addOnLoadEvent;
+}(window));
