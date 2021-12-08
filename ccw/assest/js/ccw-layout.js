532d0afd6d86c0cbb55de15f3eac16e3a8f7eba9
Yi-Cong, Kuo <yicongk@igis.com.tw>
2021-12-08 14:31:30 +0800

    fix: 頁面類型判斷
    修正原因：
    - single-page類型頁面，會被判斷成article類型頁面
    - 原來的程式會在Document.onload事件發生前執行
    - single-page類型的頁面還抓不到id為 #ccw-source-content元素，pageType就立刻運算完畢

--- ccw/assest/js/ccw-layout.js
+++ ccw/assest/js/ccw-layout.js
@@ -1,66 +1,65 @@
 (function (global, $) {
-	// initialize global variables
-	var ccw = global.ccw;
+    // initialize global variables
+    var ccw = global.ccw;
 
-	// 判斷頁面類型
-	var pageType = (function (){
-		var qstr = global.location.search,
-			ccwContent = $('#ccw-source-content').length;
+    // 判斷頁面類型
+    var getPageType = function () {
+        var qstr = global.location.search,
+            ccwContent = $('#ccw-source-content').length;
 
-		if ( qstr.includes('view=article') && ccwContent )  return 'single-page';
-		if ( qstr.includes('view=article') && !ccwContent ) return 'article';
-		if ( qstr.includes('layout=blog') )   return 'blog';
-		if ( qstr.includes('searchword=') ) return 'search';
-		
-		return "default"; // 預設值
-	}());
+        if ( qstr.includes('view=article') && ccwContent )  return 'single-page';
+        if ( qstr.includes('view=article') && !ccwContent ) return 'article';
+        if ( qstr.includes('layout=blog') )   return 'blog';
+        if ( qstr.includes('searchword=') ) return 'search';
+        
+        return "default"; // 預設值
+    };
 
+    // ============================
+    //      single-page
+    // ============================
+    var initSinglePage = function () {
+        // 搬移內容
+        $('#ccw-content').append( $('#ccw-source-content')[0].innerHTML );
+        $('#ccw-source-content').remove();
+    };
 
-	// ============================
-	//      single-page
-	// ============================
-	var initSinglePage = function () {
-		// 搬移內容
-		$('#ccw-content').append( $('#ccw-source-content')[0].innerHTML );
-		$('#ccw-source-content').remove();
-	};
+    // ============================
+    //      article
+    // ============================
+    var initArticlePage = function () {
+        // do something ...
+    };
 
-	// ============================
-	//      article
-	// ============================
-	var initArticlePage = function () {
-		// do something ...
-	};
+    // ============================
+    //      Blog
+    // ============================
+    var initBlogPage = function () {
+        // do something ...
+    };
 
-	// ============================
-	//      Blog
-	// ============================
-	var initBlogPage = function () {
-		// do something ...
-	};
+    // ============================
+    //      search
+    // ============================
+    var initSearchPage = function () {
+        // do something ...
+    };
 
-	// ============================
-	//      search
-	// ============================
-	var initSearchPage = function () {
-		// do something ...
-	};
-
-	// ============================
-	//      initialize layout
-	// ============================
-	var initLayout = function () {
-		if (pageType === 'single-page') initSinglePage();
-		if (pageType === 'article')     initArticlePage();
-		if (pageType === 'blog')        initBlogPage();
-		if (pageType === 'search')      initSearchPage();
-	};
+    // ============================
+    //      initialize layout
+    // ============================
+    var initLayout = function () {
+        if ( getPageType() === 'single-page') initSinglePage();
+        if ( getPageType() === 'article')     initArticlePage();
+        if ( getPageType() === 'blog')        initBlogPage();
+        if ( getPageType() === 'search')      initSearchPage();
+    };
 
     // ========================================================
     //      Export Modules & add Document Onload Event
     // ========================================================
     var layout = ccw.namespace('global.ccw.layout');
-    	
+        
     ccw.addOnLoadEvent(initLayout);
 
 }(window, jQuery));
